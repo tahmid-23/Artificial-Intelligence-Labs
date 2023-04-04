bb1631f4bc4e76683240a60c84bc252eede0b555
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2023-04-04 11:00:59 -0400

    add names

--- lab1_part1_starter/search_algorithms.py
+++ lab1_part1_starter/search_algorithms.py
@@ -1,5 +1,5 @@
 # Lab 1 (Part 1a and 2a)
-# Name(s): 
+# Name(s): Maxwell Gong, Tahmid Zaman
 
 from __future__ import annotations
 from typing import List, Collection, Tuple, Callable, Optional, Union, Set, Dict, Type, Iterable
